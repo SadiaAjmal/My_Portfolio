4d3e48214a7172e95a6a4bdd7f0737d47bdb0b8d
Sadia Ajmal <sadiaajmal463@gmail.com>
2024-11-26 10:43:26 +0500

    Brief description  of changes

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -5,7 +5,7 @@ import About from "../Components/About";
 const Hero = () => {
   return (
     
-      <div className="hero lg:px-34 flex xl:px-40 justify-center lg:h-[150vh] md:h-[115vh] h-[180vh] bg-custom-bg px-3 md:py-5  md:px-20 ">
+      <div className="hero lg:px-34 flex xl:px-40 justify-center lg:h-[150vh] md:h-[115vh] h-[185vh] bg-custom-bg px-3 md:py-5  md:px-20 ">
         <div className="relative md:flex  md:pt-40 pt-24 pb-10 justify-between">
           <div className=" basis-[48%] space-y-1 tracking-wider items-center justify-center flex-col">
             <p className="md:text-4xl text-3xl font-semibold ">Hello, I'm</p>
@@ -33,7 +33,7 @@ const Hero = () => {
             />
           </div>
         </div>
-      <div className=" absolute md:bottom-[-25%] bottom-[-100%] lg:bottom-[-65%]">
+      <div className=" absolute md:bottom-[-25%] bottom-[-105%] lg:bottom-[-65%]">
         <About />
       </div>
       </div>
